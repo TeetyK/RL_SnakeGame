096d746c370f33f7f3a72f28cc837c39d2009074
TeetyK <teetysupergame7484@gmail.com>
2026-01-17 10:39:08 +0700

    add: Logic game & test manual user play

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-def main():
-    print("Hello from snakegame-qlearning!")
+import src.Game
+
+def play():
+    game = src.Game.SnakeGameAI()
+    while True:
+        game_over, score = game.play_step()
+        if game_over == True:
+            break
+    
 
 
 if __name__ == "__main__":
-    main()
+    play() # I play with myself
+    # train() # AI RL play with itself
